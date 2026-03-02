c00dc58fa2e7ba585a9c08fa899534b3b79cec73
eduar <eduardolabreu@hotmail.com>
2026-03-02 10:34:20 -0300

    ajuste rota

--- CRUD/api/v1/api.py
+++ CRUD/api/v1/api.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from CRUD.api.v1.endpoints import curso
 
 api_router = APIRouter()
-api_router.include_router(curso.router, prefix='/cursos', tags=["cursos"])
+api_router.include_router(curso.router, prefix='/escola', tags=["escola"])
